13ce4e656bd0ebb469913614222031f83de4bd0c
changesunline <526625932@qq.com>
2021-03-04 18:01:53 +0800

    宏函数

--- test1/05_printnf.c
+++ test1/05_printnf.c
@@ -2,8 +2,34 @@
 // Created by zhout on 2021/3/4.
 //
 #include <stdio.h>
+#include <stdarg.h>
 
-void printnf(const char *format, ...) {
+//宏函数 - 打印当前文件路径：运行的行 运行的函数
+#define PRINTFFILELINE(format, ...) printf("("__FILE__": %d) %s: "format"\n", __LINE__, __FUNCTION__, ##__VA_ARGS__)
+#define PRINTFFILELINE_KV(value) PRINTFFILELINE(#value": %d", value)
+
+//宏函数
+#define PRINTLNF(format, ...) printf(format"\n", ##__VA_ARGS__)
+
+//普通函数
+void printLnf(const char *format, ...) {
     va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    printf("\n");
+    va_end(args);
+}
 
+void main(void) {
+//    printLnf("hello %s", "zhouting");
+//    printLnf("hello %s", "fasdfa");
+//    printLnf("hello %s", "gjgjsf");
+
+//    PRINTLNF("HELLO %s, %s", "zhangsan", "lisi");
+//    PRINTLNF("HELLO");
+//    PRINTLNF("HELLO %s, %s", "wangwu", "zhouliu");
+
+//    PRINTFFILELINE("你好 %s", "zhangsan");
+
+//    PRINTFFILELINE_KV();
 }
