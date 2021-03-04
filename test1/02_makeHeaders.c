ae12c312301ca826a10785e24131fef962b65139
changesunline <526625932@qq.com>
2021-03-04 10:34:33 +0800

    修改

--- test1/02_makeHeaders.c
+++ test1/02_makeHeaders.c
@@ -8,5 +8,5 @@
 void main(void) {
     printf("FactorialByItarator(5): %d\n", FactorialByItarator(5));
     printf("Factorial(5): %d\n", Factorial(5));
-    printf("办公室的clion设置已经完成");
+    printf("办公室的clion设置");
 }
