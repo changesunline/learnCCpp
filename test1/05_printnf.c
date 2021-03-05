2d1a09013253f1e4f2a89fc91a71d8e70aac6893
changesunline <526625932@qq.com>
2021-03-05 18:01:31 +0800

    修改

--- test1/05_printnf.c
+++ test1/05_printnf.c
@@ -4,9 +4,12 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+// 宏调用宏
+#define PRINTFLN(value, ...) printf(value"\n", ##__VA_ARGS__)
+#define PRINTFFILELINE_KV(value) PRINTFLN(#value": %d", value)
+
 //宏函数 - 打印当前文件路径：运行的行 运行的函数
 #define PRINTFFILELINE(format, ...) printf("("__FILE__": %d) %s: "format"\n", __LINE__, __FUNCTION__, ##__VA_ARGS__)
-#define PRINTFFILELINE_KV(value) PRINTFFILELINE(#value": %d", value)
 
 //宏函数
 #define PRINTLNF(format, ...) printf(format"\n", ##__VA_ARGS__)
@@ -31,5 +34,6 @@ void main(void) {
 
 //    PRINTFFILELINE("你好 %s", "zhangsan");
 
-//    PRINTFFILELINE_KV();
+    int abc = 3;
+    PRINTFFILELINE_KV(++abc);
 }
