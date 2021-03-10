7e9b24e248917d6230edebcd32bcd07c06322a97
changesunline <526625932@qq.com>
2021-03-10 18:01:16 +0800

    指针解析

--- test1/07_pointer.c
+++ test1/07_pointer.c
@@ -5,13 +5,29 @@
 #include <io_utils.h>
 
 void main(void) {
-    int a = 10;
+//    int a = 10;
 //    scanf("%d", &a);
 
+//    int *p = &a;
+//    int **pp = &p;
+//    PRINT_HEX(p);
+//    PRINT_HEX(&a);
+//    PRINT_INT(a);
+//    PRINT_INT(*p);
+
+//    PRINT_HEX(*pp);
+//    PRINT_HEX(p);
+
+//    int *const cp = &a;
+//    int const *const cpp = &a;
+//    int const *cppp = &a;
+
+//    PRINT_INT(*cp);
+//    PRINT_INT(*cp);
+//    PRINT_INT(cppp);
+//    PRINT_INT(a);
+    int a = 5;
     int *p = &a;
-    int **pp = &*p;
+    *p = 2;
     PRINT_HEX(p);
-    PRINT_HEX(&a);
-    PRINT_INT(a);
-    PRINT_INT(*p);
 }
